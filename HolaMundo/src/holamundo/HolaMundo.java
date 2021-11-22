ef554728641611470f08a209d951428b04382437
Javi <dawjavi@gmail.com>
2021-11-22 19:33:01 +0100

    Creacion de Hola Mundo v 1.1

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -19,6 +19,9 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Esta es la version 1.1");
     }
 
 }
+
