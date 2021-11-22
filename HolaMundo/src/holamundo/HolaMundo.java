e5955cc3cb5e3019b667920513faef1d8bd55767
Javi <dawjavi@gmail.com>
2021-11-22 19:31:00 +0100

    Creacion de Hola Mundo v 1.0

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto $(project.name} - Archivo HolaMundo.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0
+ */
+
+package holamundo;
+
+/**
+ *
+ * @author Javier Noguera Giménez<dawjavi@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:07:40
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
